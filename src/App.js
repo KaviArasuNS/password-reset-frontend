1d65b76921fb346f95b0185f0a87896eae100021
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-31 13:40:20 +0530

    password reset flow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import EmailVerify from "./components/EmailVerify/EmailVerify";
+import Login from "./components/Login/Login";
+import Main from "./components/Main/main";
+import Signup from "./components/Signup/Signup";
 
 function App() {
+  const user = localStorage.getItem("token");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/" element={<Navigate replace to="/login" />} />
+      <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+    </Routes>
   );
 }
 
