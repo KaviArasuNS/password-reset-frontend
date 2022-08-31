951d0ac70779b650a1d28a7c0f577312ddf2732d
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-31 18:40:07 +0530

    password reset flow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import EmailVerify from "./components/EmailVerify/EmailVerify";
+import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import Login from "./components/Login/Login";
 import Main from "./components/Main/main";
+import PasswordReset from "./components/PasswordReset/PasswordReset";
 import Signup from "./components/Signup/Signup";
 
 function App() {
@@ -14,6 +16,8 @@ function App() {
       <Route path="/login" exact element={<Login />} />
       <Route path="/" element={<Navigate replace to="/login" />} />
       <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/passwore-reset/:id/:token" element={<PasswordReset />} />
     </Routes>
   );
 }
